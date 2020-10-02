86536efeb06e1acfde772d2d5c66df588a992a6c
Varsha KR <43178958+varshakr1298@users.noreply.github.com>
2020-10-02 17:29:15 +0530

    Day 15 Challenge Solved

---
+++ day15.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class day15 {
+	private static final Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String input;
+		System.out.print("Enter the input string number: ");
+		input=scanner.next();
+		Long n = Long.parseLong(input);
+		System.out.print("The output integer is as follows: "+n);
+	}
+
+}
