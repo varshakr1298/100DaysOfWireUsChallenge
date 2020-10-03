22ec02ceebba705ea2352d44896e1156e9b60a67
Varsha KR <43178958+varshakr1298@users.noreply.github.com>
2020-10-03 18:13:17 +0530

    Updated Day 15 Challenge Code

--- day15.java
+++ day15.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 
 public class day15 {
 	private static final Scanner scanner = new Scanner(System.in);
+		public static int convertStringToInt(String input){
+		// Write your code here
+        if(input.length()==0)
+        {
+          return 0;
+        }
+      int getnum=input.charAt(input.length()-1)-'0';
+      return getnum+(10*(convertStringToInt(input.substring(0,input.length()-1))));
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -10,6 +19,8 @@ public class day15 {
 		input=scanner.next();
 		Long n = Long.parseLong(input);
 		System.out.print("The output integer is as follows: "+n);
+		int result=convertStringToInt(input);
+		System.out.print("The reulting integer is as follows by traditional method: "+result);
 	}
 
 }
